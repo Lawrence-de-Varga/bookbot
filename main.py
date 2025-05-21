d4316ce7f316b1b0d859c71e28c0703d395bc63f
Lawrence de Varga <lawrencedevarga@gmail.com>
2025-05-21 10:51:14 +1000

    counting chars and punctuation.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_alpha_chars, count_punc_chars
 
 def get_book_text(book_path):
     with open(book_path) as b:
@@ -9,8 +9,10 @@ def get_book_text(book_path):
 def main():
     franken = get_book_text("books/frankenstein.txt")
     word_count = count_words(franken)
-    char_count = count_characters(franken)
+    char_count = count_alpha_chars(franken)
+    punc_count = count_punc_chars(franken)
     # print(f"{char_count} charactersfound in the document")
     print(char_count)
-
+    print()
+    print(punc_count)
 main()    
--- stats.py
+++ stats.py
@@ -1,15 +1,29 @@
+import re
+import string
+
+alpha_chars = re.compile(r'[a-zA-Z]')
+punc_chars = re.compile(f'[{re.escape(string.punctuation)}]')
+
 def count_words(book_text):
     return len(book_text.split())
 
-def count_characters(text):
+
+def count_chars(text, char_regex):
     chars = list(text)
     chars = list(map(str.lower, chars))
 
     char_count = {}
     for char in chars:
-        if char in char_count:
+        if char in char_count and re.match(char_regex, char):
             char_count[char] += 1
-        else:
+        elif re.match(char_regex, char):
             char_count[char] = 1
 
     return char_count
+
+
+def count_alpha_chars(text):
+    return count_chars(text, alpha_chars)
+
+def count_punc_chars(text):
+    return count_chars(text, punc_chars)
